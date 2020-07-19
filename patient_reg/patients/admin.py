e11a935b3ffe8d7b87e91ba47ee16c945e309dac
Xiaoli <nsnq@qq.com>
2020-07-19 21:34:42 +0800

    display Patient name on list view within admin panel

--- patient_reg/patients/admin.py
+++ patient_reg/patients/admin.py
@@ -7,7 +7,13 @@ class PatientAdmin(admin.ModelAdmin):
     ordering = ['-created_at', '-updated_at']
 
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('due_date',) 
+    list_display = ('get_patient_name', 'due_date',) 
+
+    def get_patient_name(self, obj):
+        return ('%s %s' % (obj.patient.first_name, obj.patient.last_name))
+
+    get_patient_name.admin_order_field  = 'patient'  #Allows column order sorting
+    get_patient_name.short_description = 'Patient Name'  #Renames column head
 
 admin.site.register(Patient, PatientAdmin)
 admin.site.register(Appointment, AppointmentAdmin)
