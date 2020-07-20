7f534cc3d499dd3e89f7b040756bb6580735232c
Xiaoli <nsnq@qq.com>
2020-07-20 16:02:13 +0800

    fixed path of uploading files

--- patient_reg/patients/views.py
+++ patient_reg/patients/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
-from .models import Patient, Appointment
+from django.core.files.storage import FileSystemStorage
 
 from datetime import datetime
 import dateutil.parser
+
+from .models import Patient, Appointment
 
 @csrf_exempt
 def register(request):
     response = {'status':'ok'}
     
+    # recieving form data
     first_name = request.POST.get('first_name')
     last_name = request.POST.get('last_name')
     date_of_birth = request.POST.get('date_of_birth')
@@ -20,6 +22,7 @@ def register(request):
     dueTime = request.POST.get('dueTime')
     photoFile = request.FILES['file']
 
+    # handing date & time data
     try:
         valid_birth_date = datetime.strptime(request.POST['date_of_birth'], '%Y-%m-%d')
         valid_due_time = dateutil.parser.parse(request.POST['dueTime'])
@@ -27,13 +30,19 @@ def register(request):
         # handle this
         pass
 
+    # handing photo ID
+    photo_file = photoFile
+    #fs = FileSystemStorage()
+    #filename = fs.save(id_photo.name, photo_id_file)
+    #uploaded_file_url = fs.url(filename)
+
+    # saving Patient object
     p = Patient(first_name=first_name, last_name=last_name, birth_date=valid_birth_date)
     p.phone_number = phone
     p.email = email
     p.address = address
     p.due_date = valid_due_time
-    id_photo = photoFile
+    p.id_photo = photo_file
     p.save()
 
     return JsonResponse(response)
-
